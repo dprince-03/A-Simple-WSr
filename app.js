bd43521f12fa4953fcd87f16841b91bda2a2412e
dprince-03 <princeadedayo03@gmail.com>
2025-08-22 15:02:32 +0100

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+
+async function fetchWebData() {
+    console.log('Fetching data from: ...nothing yet...');
+    
+}
+
+fetchWebData();
