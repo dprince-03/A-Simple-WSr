b302ad9322f23356fbcb213953f48fffc6c207fd
dprince-03 <princeadedayo03@gmail.com>
2025-08-23 23:45:48 +0100

    app update

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const cheerio = require('cheerio');
-const fetch = require('node-fetch');
+import * as cheerio from 'cheerio';
+import fetch from 'node-fetch';
 
 async function fetchWebData() {
-    console.log('Fetching data from: ...nothing yet...');
+    try {
+			const response = await fetch(
+				"https://github.com/dprince-03/A-Simple-WSr"
+			);
+			const body = await response.text();
+			const $ = cheerio.load(body);
+            
+			// const wrapper = $(".px-3");
+			// console.log(wrapper);
+			// console.log(wrapper.length);
+
+			const items = [];
+			$(".px-3").map((i, el) => {});
+            
+		} catch (error) {
+        console.error('Error fetching data:', error);
+        return;
+    }
+    // console.log('Fetching data from: ...nothing yet...');
     
 }
 
